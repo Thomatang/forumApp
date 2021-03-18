685aa93e829eacbe57b6374a5c754fc37da26c0b
Thomas Bouchard <thomaswardbouchard@gmail.com>
2021-03-18 10:24:30 +0100

    MOD: run eslint auto fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,14 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
+    'plugin:vue/vue3-essential',
     '@vue/standard'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
-    "no-unused-vars" : process.env.NODE_ENV === "production" ? "error" : "warn",
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
