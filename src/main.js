74f99999df299077b715fd29f26ff115fe206fc6
Thomas Bouchard <thomaswardbouchard@gmail.com>
2021-03-18 15:14:24 +0100

    ADD: dynamic routing and route param as props

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import PageHome from '@/components/PageHome'
+import PageThreadShow from '@/components/PageThreadShow'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: PageHome
+  },
+  {
+    path: '/thread/:id',
+    name: 'Thread',
+    component: PageThreadShow,
+    props: true
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 const forumApp = createApp(App)
-
+forumApp.use(router)
 forumApp.mount('#app')
