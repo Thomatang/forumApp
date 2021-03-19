6883198cb6bf5c1cfc3150022c1698e8fa6f08f9
Thomas Bouchard <thomaswardbouchard@gmail.com>
2021-03-19 11:00:36 +0100

    ADD: soft 404 page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="col-full  text-center">
+    <h1>Page not found.</h1>
+    <router-link :to="{name:'Home'}">Read some cool threads!</router-link>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
