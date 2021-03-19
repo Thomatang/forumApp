623bfe4e67200092f2476804b89de290a543fa54
Thomas Bouchard <thomaswardbouchard@gmail.com>
2021-03-19 19:26:01 +0100

    MOD: app organisation, DEL: vue logo, ADD: Route guard to redirect if inexistant content

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-import PageNotFound from '@/components/PageNotFound'
+import Home from '@/pages/Home'
+import PageThreadShow from '@/pages/ThreadShow'
+import PageNotFound from '@/pages/NotFound'
+import sourceData from '@/data.json'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: PageHome
+    component: Home
   },
   {
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter: (to, from, next) => {
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      if (threadExists) {
+        next()
+      } else {
+        next({
+          name: 'PageNotFound',
+          // preserve current path and remove the first char to avoid the target URL starting with `//`
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash if any
+          query: this.$route.query,
+          hash: this.$route.hash
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
