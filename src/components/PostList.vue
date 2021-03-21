16418664bb78313c76e2888de7c40bdd404b8a10
Thomas Bouchard <thomaswardbouchard@gmail.com>
2021-03-21 17:54:41 +0100

    ADD: dayjs module, ADD: human readable dates, relativeTime and hover feature

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -21,14 +21,19 @@
                       </div>
                   </div>
 
-                  <div class="post-date text-faded">
-                      {{post.publishedAt}}
+                  <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)">
+                      {{ diffForHumans(post.publishedAt) }}
                   </div>
               </div>
           </div>
 </template>
 <script>
 import sourceData from '@/data.json'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedDate from 'dayjs/plugin/localizedFormat'
+dayjs.extend(relativeTime)
+dayjs.extend(localizedDate)
 
 export default {
   props: {
@@ -37,14 +42,20 @@ export default {
       type: Array
     }
   },
-  methods: {
-    userById (userId) {
-      return this.users.find(u => u.id === userId)
-    }
-  },
   data () {
     return {
       users: sourceData.users
+    }
+  },
+  methods: {
+    userById (userId) {
+      return this.users.find(u => u.id === userId)
+    },
+    diffForHumans (timestamp) {
+      return dayjs.unix(timestamp).fromNow()
+    },
+    humanFriendlyDate (timestamp) {
+      return dayjs.unix(timestamp).format('llll')
     }
   }
 }
